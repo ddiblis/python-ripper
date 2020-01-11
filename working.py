b654468071a4be48394ec11590c9bc02f0fb91dc
thealiengunner <thealiengunner@penguin>
2020-01-10 21:05:14 -0700

    Added pages to Chapter

--- working.py
+++ working.py
@@ -2,11 +2,22 @@
 
 from bs4 import BeautifulSoup as BS
 import requests
+from tqdm import tqdm
+from zipfile import ZipFile
+import os.path
+from urllib.parse import urlparse
 from urllib import parse
 
 websitename = "https://www.mangareader.net/"
 chapter_url = "https://www.mangareader.net/goblin-is-very-strong/18"
 
+output = urlparse(chapter_url).path
+
+for splitoutput in output:
+    splitpath = os.path.split(output) 
+    serieswithslash = splitpath[0].split("/") 
+    series = serieswithslash[1] 
+    chapter_number = splitpath[1] 
 
 class WebPage:
     def __init__(self, url):
@@ -48,11 +59,22 @@ class Chapter(WebPage):
     def generate_page(self, url):
         return Page(url)
 
+    def download(self, ): 
+        pages = self.pages()
+        with ZipFile(f"{series}  {chapter_number} .cbz", "w") as zf:     
+             for num, p in tqdm(enumerate(pages, 1), total=len(pages), desc=f"text"): 
+                 with zf.open(f"{series}  {chapter_number} {num:02}.jpg", "w") as f: 
+                     resp = requests.get(p.image) 
+                     f.write(resp.content) 
+
     @property
     def page_list(self):
         if self._page_list is None:
             self._page_list = self.enumerate_pages()
         return self._page_list
+    
+    def pages(self):
+        return [self.generate_page(p) for p in self.page_list]
 
 
 class Series(WebPage):
