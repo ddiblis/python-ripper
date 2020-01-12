ce11ecbb619da6814c5567e27ffa5cc6ce5adc5d
thealiengunner <thealiengunner@penguin>
2020-01-11 17:53:35 -0700

    fixed file name and rearranged

--- working.py
+++ working.py
@@ -30,61 +30,6 @@ class WebPage:
         return self._soup
 
 
-class Page(WebPage):
-    @property
-    def image(self):
-        img = self.soup.find("img", id="img")
-        return img.attrs["src"]
-
-
-class Chapter(WebPage):
-    def __init__(self, url):
-        self._page_list = None
-        super().__init__(url)
-
-    def enumerate_pages(self):
-        pages = self.soup.select("div#selectpage option")
-        return [
-            parse.urljoin(websitename, page.attrs["value"]) for page in pages
-        ]
-
-    def download(self,):
-        pages = self.pages()
-        with ZipFile(f"{self.series}_{self.number}.cbz", "w") as zf:
-            for num, p in tqdm(
-                enumerate(pages, 1),
-                total=len(pages),
-                desc=f"{self.series}-{self.number}",
-            ):
-                with zf.open(f"{num:02}.jpg", "w") as f:
-                    resp = requests.get(p.image)
-                    f.write(resp.content)
-
-    @property
-    def info(self):
-        return re.search(r".*/(?P<series>[^/]*)/(?P<num>\d*)", self.url)
-
-    @property
-    def number(self):
-        return self.info["num"]
-
-    @property
-    def page_list(self):
-        if self._page_list is None:
-            self._page_list = self.enumerate_pages()
-        return self._page_list
-
-    @property
-    def series(self):
-        return self.info["series"]
-
-    def generate_page(self, url):
-        return Page(url)
-
-    def pages(self):
-        return [self.generate_page(p) for p in self.page_list]
-
-
 class Series(WebPage):
     def __init__(self, url):
         self._chapter_list = None
@@ -108,3 +53,58 @@ class Series(WebPage):
         if self._chapter_list is None:
             self._chapter_list = self.enumerate_chapters()
         return self._chapter_list
+
+
+class Chapter(WebPage):
+    def __init__(self, url):
+        self._page_list = None
+        super().__init__(url)
+
+    def enumerate_pages(self):
+        pages = self.soup.select("div#selectpage option")
+        return [
+            parse.urljoin(websitename, page.attrs["value"]) for page in pages
+        ]
+
+    def download(self,):
+        pages = self.pages()
+        with ZipFile(f"{self.series} Ch.{self.number}.cbz", "w") as zf:
+            for num, p in tqdm(
+                enumerate(pages, 1),
+                total=len(pages),
+                desc=f"{self.series} Ch.{self.number}",
+            ):
+                with zf.open(f"{num:03}.jpg", "w") as f:
+                    resp = requests.get(p.image)
+                    f.write(resp.content)
+
+    @property
+    def info(self):
+        return re.search(r".*/(?P<series>[^/]*)/(?P<num>\d*)", self.url)
+
+    @property
+    def number(self):
+        return self.info["num"]
+
+    @property
+    def page_list(self):
+        if self._page_list is None:
+            self._page_list = self.enumerate_pages()
+        return self._page_list
+
+    @property
+    def series(self):
+        return self.info["series"].replace("-", " ")
+
+    def generate_page(self, url):
+        return Page(url)
+
+    def pages(self):
+        return [self.generate_page(p) for p in self.page_list]
+
+
+class Page(WebPage):
+    @property
+    def image(self):
+        img = self.soup.find("img", id="img")
+        return img.attrs["src"]
